aa82d44e7316e338b830cca7f69d2ed5245321b3
rajesh <rajnayak91@gmail.com>
2019-09-20 13:36:24 +0530

    react-redux added

--- src/main/java/com/train/Application.java
+++ src/main/java/com/train/Application.java
@@ -9,10 +9,10 @@ public class Application {
 
     public static void main(String args[]) {
 
-        //System.setProperty("spring.profiles.active","inMemoryDataService");
+        System.setProperty("spring.profiles.active","inMemoryDataService");
         //System.setProperty("spring.profiles.active","fileDataService");
        // System.setProperty("spring.profiles.active", "MsAccessDataService");
-        System.setProperty("spring.profiles.active", "H2DBDataService");
+      //  System.setProperty("spring.profiles.active", "H2DBDataService");
         //System.setProperty("spring.profiles.active","JpaDataService");
         SpringApplication.run(Application.class, args);
 
